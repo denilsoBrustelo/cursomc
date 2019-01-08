2954d748dd8fa97e46f81a06dfa92230961f53dd
Denilso F. Brustelo <denilso.brustelo@gmail.com>
2019-01-08 17:10:27 -0200

    ItemPedido e ItemPedidoPk

---
+++ src/main/java/com/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,13 @@
+package com.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Serializable> {
+
+}
