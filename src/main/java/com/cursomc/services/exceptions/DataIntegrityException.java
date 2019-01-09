e07f0e5c5afffd50247174bc9150126ea9dde60b
Denilso F. Brustelo <denilso.brustelo@gmail.com>
2019-01-09 14:49:41 -0200

    Deletando um Categoria com DELETE

---
+++ src/main/java/com/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,18 @@
+package com.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String message) {
+		super(message);
+	}
+
+	public DataIntegrityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
